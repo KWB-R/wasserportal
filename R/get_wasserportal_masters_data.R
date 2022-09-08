49f0cdc76672fb598f3cd2d631834fec04d09a84
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-08 06:45:00 +0200

    Clean get_wasserportal_masters_data()

--- R/get_wasserportal_masters_data.R
+++ R/get_wasserportal_masters_data.R
@@ -13,21 +13,21 @@
 #' parallel::detectCores()
 #' master_urls <- stations$overview_list$surface_water.water_level$stammdaten_link
 #' system.time(get_wasserportal_masters_data(master_urls))
-#' system.time(get_wasserportal_masters_data(master_urls,
-#'                                           run_parallel = FALSE))
+#' system.time(get_wasserportal_masters_data(master_urls, run_parallel = FALSE))
 #'
 get_wasserportal_masters_data <- function(
-  master_urls,
-  run_parallel = TRUE
+    master_urls,
+    run_parallel = TRUE
 )
 {
-  msg <- sprintf("Importing %d station metadata from Wasserportal Berlin",
-                 length(master_urls))
-
+  msg <- sprintf(
+    "Importing %d station metadata from Wasserportal Berlin",
+    length(master_urls)
+  )
 
   if (run_parallel) {
 
-    ncores <- parallel::detectCores() - 1
+    ncores <- parallel::detectCores() - 1L
 
     cl <- parallel::makeCluster(ncores)
 
@@ -35,7 +35,7 @@ get_wasserportal_masters_data <- function(
       messageText = msg,
       expr = parallel::parLapply(
         cl, master_urls, function(master_url) {
-          try(wasserportal::get_wasserportal_master_data(master_url = master_url))
+          try(wasserportal::get_wasserportal_master_data(master_url))
         })
     )
 
@@ -44,11 +44,10 @@ get_wasserportal_masters_data <- function(
   } else {
 
     master_list <- lapply(master_urls, function(master_url) {
-      wasserportal::get_wasserportal_master_data(master_url)
-      })
+      try(wasserportal::get_wasserportal_master_data(master_url))
+    })
 
   }
 
   data.table::rbindlist(master_list, fill = TRUE)
-
 }
