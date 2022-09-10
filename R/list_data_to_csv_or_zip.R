58baee62c740fc53ad0af1d17c017cd2a49998b4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-10 05:14:34 +0200

    Require to set arguments explicitly
    Do not use defaults in inner function.
    This makes the difference between
    
    - list_timeseries_data_to_zip() and
    - list_masters_data_to_csv()
    
    more obvious.

--- R/list_data_to_csv_or_zip.R
+++ R/list_data_to_csv_or_zip.R
@@ -1,18 +1,14 @@
-#' Helper function: list data to csv
+#' Helper function: list data to csv or zip
 #'
 #' @param data_list data in list form
-#' @param file_prefix file prefix (default: "")
-#' @param to_zip convert to zip file (default: FALSE)
+#' @param file_prefix file prefix
+#' @param to_zip whether or not to convert to zip file
 #' @return loops through list of data frames and uses list names as filenames
 #' @export
 #' @importFrom archive archive_write
 #' @importFrom readr write_csv
 #' @importFrom stringr str_replace str_replace_all
-list_data_to_csv_or_zip <- function(
-    data_list,
-    file_prefix = "",
-    to_zip = FALSE
-)
+list_data_to_csv_or_zip <- function(data_list, file_prefix, to_zip)
 {
   tmp <- lapply(names(data_list), function(name) {
 
