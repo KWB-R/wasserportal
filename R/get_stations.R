1db01fc3d4a50e2937971b651b4a40a9bfa8b6e2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-09-24 08:08:14 +0200

    Make "type" the main (first) argument

--- R/get_stations.R
+++ R/get_stations.R
@@ -1,14 +1,14 @@
 #' Get Stations
 #'
-#' @param run_parallel default: TRUE
-#' @param n_cores number of cores to use if \code{run_parallel = TRUE}.
-#'   Default: one less than the detected number of cores.
-#' @param debug logical indicating whether or not to show debug messages
 #' @param type vector of character describing the type(s) of output(s) to be
 #'   returned. Expected values (and default): \code{c("list", "data.frame",
 #'   "crosstable")}. If only one value is given the data is returned in the
 #'   expected type. If more than one values are given, a list is returned with
 #'   one list element per type.
+#' @param run_parallel default: TRUE
+#' @param n_cores number of cores to use if \code{run_parallel = TRUE}.
+#'   Default: one less than the detected number of cores.
+#' @param debug logical indicating whether or not to show debug messages
 #' @return list with general station "overview" (either as list "overview_list"
 #' or as data.frame "overview_df") and a crosstable with information which
 #' parameters is available per station ("x" if available, NA if not)
@@ -24,10 +24,10 @@
 #' str(stations)
 #'
 get_stations <- function(
+    type = c("list", "data.frame", "crosstable"),
     run_parallel = TRUE,
     n_cores = parallel::detectCores() - 1L,
-    debug = TRUE,
-    type = c("list", "data.frame", "crosstable")
+    debug = TRUE
 )
 {
   expected_types <- c("list", "data.frame", "crosstable")
