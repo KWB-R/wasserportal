2d7fe905fbdd89991152aa3a7b5db480761d5aa0
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-10-10 14:14:52 +0200

    Handle NA in get_non_external_station_ids()
    and improve the readability of the test

--- tests/testthat/test-function-get_non_external_station_ids.R
+++ tests/testthat/test-function-get_non_external_station_ids.R
@@ -8,12 +8,20 @@ test_that("get_non_external_station_ids() works", {
 
   portal_url <- "https://wasserportal.berlin.de"
 
-  station_data <- kwb.utils::noFactorDataFrame(
-    Messstellennummer = as.character(1:4),
-    Betreiber =       c("any", "any", "Land Berlin", "Land Berlin"),
-    stammdaten_link = c("any", portal_url, "any", portal_url)
+  station_data <- read.table(sep = ",", header = TRUE, text = "
+    Messstellennummer,Betreiber,stammdaten_link
+    1,any,any
+    2,any,https://wasserportal.berlin.de
+    3,Land Berlin,any
+    4,Land Berlin,https://wasserportal.berlin.de
+    5,,https://wasserportal.berlin.de"
   )
+
+  is_empty <- station_data$Betreiber == ""
 
   expect_identical(f(station_data), "4")
 
+  station_data$Betreiber[is_empty] <- NA
+
+  expect_identical(f(station_data), "4")
 })
